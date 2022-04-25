c7e5a2d6260ec7f1372084c531377177bd83aef6
Rheditia Ferdiansyah <rheditiaf2406@gmail.com>
2022-04-25 17:41:01 +0700

    Church Assets & Input Handler
    - Added Gothicvania Church assets by Anzimus
    - Added Base Input Handler for Player Move

---
+++ Assets/_Project/_Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    PlayerInput input;
+    InputAction moveAction;
+
+    public Vector2 MoveInput { get; private set; }
+
+    private void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+        moveAction = input.actions["Move"];
+    }
+
+    private void OnEnable()
+    {
+        moveAction.started += OnMoveInput;
+        moveAction.performed += OnMoveInput;
+        moveAction.canceled += OnMoveInput;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.started -= OnMoveInput;
+        moveAction.performed -= OnMoveInput;
+        moveAction.canceled -= OnMoveInput;
+    }
+
+    private void OnMoveInput(InputAction.CallbackContext context)
+    {
+        MoveInput = context.ReadValue<Vector2>();
+        Debug.Log(MoveInput);
+    }
+}
