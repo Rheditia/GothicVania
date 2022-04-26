181dced4ae3a21dbce5da2b83faecf29407fb2ed
Rheditia Ferdiansyah <rheditiaf2406@gmail.com>
2022-04-26 17:45:43 +0700

    Basic Jump
    - Added placeholder Jump mechanic on Locomotion Script
    - Added Jump & Fall Animation with blend tree

--- Assets/_Project/_Scripts/Player/PlayerInputHandler.cs
+++ Assets/_Project/_Scripts/Player/PlayerInputHandler.cs
@@ -8,13 +8,16 @@ public class PlayerInputHandler : MonoBehaviour
 {
     PlayerInput input;
     InputAction moveAction;
+    InputAction jumpAction;
 
     public Vector2 MoveInput { get; private set; }
+    public bool JumpInput { get; private set; }
 
     private void Awake()
     {
         input = GetComponent<PlayerInput>();
         moveAction = input.actions["Move"];
+        jumpAction = input.actions["Jump"];
     }
 
     private void OnEnable()
@@ -22,6 +25,9 @@ public class PlayerInputHandler : MonoBehaviour
         moveAction.started += OnMoveInput;
         moveAction.performed += OnMoveInput;
         moveAction.canceled += OnMoveInput;
+
+        jumpAction.started += OnJumpInput;
+        jumpAction.canceled += OnJumpInput;
     }
 
     private void OnDisable()
@@ -29,11 +35,20 @@ public class PlayerInputHandler : MonoBehaviour
         moveAction.started -= OnMoveInput;
         moveAction.performed -= OnMoveInput;
         moveAction.canceled -= OnMoveInput;
+
+        jumpAction.started -= OnJumpInput;
+        jumpAction.canceled -= OnJumpInput;
     }
 
     private void OnMoveInput(InputAction.CallbackContext context)
     {
         MoveInput = context.ReadValue<Vector2>();
         //Debug.Log(MoveInput);
+    }
+
+    private void OnJumpInput(InputAction.CallbackContext context)
+    {
+        JumpInput = context.ReadValue<float>() == 1;
+        //Debug.Log(JumpInput);
     }
 }
--- Assets/_Project/_Scripts/Player/PlayerLocomotion.cs
+++ Assets/_Project/_Scripts/Player/PlayerLocomotion.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,22 @@ public class PlayerLocomotion : MonoBehaviour
 {
     Rigidbody2D myRigidbody;
 
+    PlayerInputHandler inputHandler;
+    [SerializeField] float groundCheckRadius = 0f;
+    [SerializeField] Vector3 groundCheckOffset;
+    [SerializeField] LayerMask platformLayer;
+    [SerializeField] float jumpSpeed = 0f;
+
     private void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+
+        inputHandler = GetComponent<PlayerInputHandler>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (inputHandler.JumpInput) { Jump(); }
     }
 
     public void SetHorizontalVelocity(float velocity, float horizontalInput)
@@ -22,5 +36,23 @@ public class PlayerLocomotion : MonoBehaviour
     {
         if (Mathf.Abs(input) <= Mathf.Epsilon) { return; }
         transform.localScale = new Vector3(Mathf.Sign(myRigidbody.velocity.x), 1, 1);
+    }
+
+    private void Jump()
+    {
+        if (!CheckIfGrounded()) { return; }
+        Vector2 newVelocity = new Vector2(myRigidbody.velocity.x, jumpSpeed);
+        myRigidbody.velocity = newVelocity;
+    }
+
+    private bool CheckIfGrounded()
+    {
+        return Physics2D.OverlapCircle(transform.position + groundCheckOffset, groundCheckRadius, platformLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position + groundCheckOffset, groundCheckRadius);
     }
 }
